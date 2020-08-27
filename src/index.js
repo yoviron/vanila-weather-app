9eb1beefe6d745bfa3e584ee9e2fb462a734ce8e
yoviron <yoviron@yahoo.com>
2020-08-27 18:56:39 -0400

    creating display temperature

--- src/index.js
+++ src/index.js
@@ -16,9 +16,28 @@ if (minutes <10) {
 
 }
 if (hours<10) {
-    hours= `0${hotrs}`;
+    hours= `0${hours}`;
 }
+function showWeather(response) {
+    console.log(response.data);
+    
+    
+    let temperatureElement= document.querySelector("#temperature");
+    let cityElement= document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windElement=document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    cityElement.innerHTML=response.data.name;
+    descriptionElement.innerHTML=response.data.weather[0].description;
+    humidityElement.innerHTML=response.data.main.humidity;
+    windElement.innerHTML=response.data.wind.speed;
+}    
+
+
 
 
 let apiKey = "3e050f75e6d0f064cfedf4c3fb91df60";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q={#city}&appid={apiKey}&units=metric`;
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=Singapore&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showWeather);
